ba9a04658b21cd75a36e06132cd5c4462054128b
Iyehvah <Iyehvah@users.noreply.github.com>
2019-12-15 21:34:03 -0600

    Going to try installing some jest stuff but dont want to mess up my code..

---
+++ client/src/App.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+//passes but throws my whole data.js information into the test for some reason.. blah
+// describe('testing...', () => {
+//     describe('first test', () => {
+//         test('app does render!' , () => {
+//             const div = document.createElement('div');
+//             ReactDOM.render(<App />, div);
+//         })
+//     })
+// })
+
